2d1952c15a58078aad2aa7debf1b2c9611314dc9
Sergei <df.rootkit@gmail.com>
2016-05-27 17:44:56 +0300

    clean up the code

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express')
 var path = require('path')
-// var favicon = require('serve-favicon');
+//  var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
@@ -24,9 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.use('/', routes)
 app.use('/users', users)
 
-//app.listen(3000, function () {
-  //console.log('express has started on port 3000')
-//})
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found')
@@ -35,7 +32,6 @@ app.use(function (req, res, next) {
 })
 
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
