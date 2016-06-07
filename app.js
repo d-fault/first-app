66068d2188a3005acc82d478e851c82133cf4f43
Sergei <df.rootkit@gmail.com>
2016-06-07 13:12:54 +0300

    dev branch commit test. Trouble with filling data in the table

--- app.js
+++ app.js
@@ -1,27 +1,23 @@
 var express = require('express')
 var path = require('path')
-// var favicon = require('serve-favicon')
 var logger = require('morgan')
 var cookieParser = require('cookie-parser')
 var bodyParser = require('body-parser')
 var admin = require('./routes/admin')
 var app = express()
+var mybase = 'mongodb://localhost/myappdatabase'
 var mongoose = require('mongoose')
-var User = require('./models/userschema')
+// var User = require('./models/userschema')
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'jade')
-
-// uncomment after placing your favicon in /public
-// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
-app.use('/', admin)
-
+app.use('/admin', admin) // get the app router
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found')
@@ -54,22 +50,18 @@ app.use(function (err, req, res, next) {
 
 // Here we find an appropriate database to connect to, defaulting to
 // localhost if we don't find one.
-var uristring =
-process.env.MONGOLAB_URI ||
-process.env.MONGOHQ_URL ||
-'mongodb://localhost/myappdatabase'
-mongoose.connect(uristring, function (err, res) {
+mongoose.connect(mybase, function (err, res) {
   if (err) {
-    console.log('ERROR connecting to: ' + uristring + '. ' + err)
+    console.log('ERROR connecting to: ' + mybase + '. ' + err)
   } else {
-    console.log('Succeeded connected to: ' + uristring)
+    console.log('connected to: ' + mybase)
   }
 })
 
+/*
 // get the user player
 User.find({ username: 'player' }, function (err, user) {
   if (err) throw err
-
   // delete him
   User.remove(function (err) {
     if (err) throw err
@@ -90,13 +82,6 @@ player.save(function (err) {
   if (err) throw err
   console.log('User saved successfully!')
 })
+*/
 
-// get all the users
-User.find({}, function (err, users) {
-  if (err) throw err
-  // object of all the users
-  console.log(users)
-})
-
-// it works=)
 module.exports = app
--- routes/admin.js
+++ routes/admin.js
@@ -1,25 +1,23 @@
 var express = require('express')
 var router = express.Router()
+var User = require('../models/userschema')
 
 /* GET home page. */
-router.get('/admin', function (req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'death star control pannel' })
 })
 
 /* GET users listing. */
-router.post('/admin/users', function (req, res, next) {
+router.post('/users', function (req, res, next) {
   res.render('users', {title: 'USER ACCOUNTS'})
 })
-/*
- * POST to adduser.
- */
-router.post('/adduser', function (req, res) {
-  var db = req.db
-  var collection = db.get('userlist')
-  collection.insert(req.body, function (err, result) {
-    res.send(
-            (err === null) ? { msg: '' } : { msg: err }
-        )
+
+router.get('/users/test', function (req, res, next) {
+  User.find({}, function (err, users) {
+    if (err) throw err
+    // object of all the users
+    // console.log(users)
+    res.json(users)
   })
 })
 
